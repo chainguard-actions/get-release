859ab958e90c609bfcedde9484c7bc484efae27a
actionchain <actionchain@chainguard.dev>
2026-05-12 21:42:29 +0000

    Update joutvhu/get-release@v1.0.3

---
+++ src/constants.ts
@@ -0,0 +1,27 @@
+export enum Inputs {
+    Owner = 'owner',
+    Repo = 'repo',
+    TagName = 'tag_name',
+    Latest = 'latest',
+    Pattern = 'pattern',
+    PreRelease = 'prerelease',
+    Debug = 'debug',
+    Throwing = 'throwing'
+}
+
+export enum Outputs {
+    Id = 'id',
+    NodeId = 'node_id',
+    Url = 'url',
+    HtmlUrl = 'html_url',
+    AssetsUrl = 'assets_url',
+    UploadUrl = 'upload_url',
+    TagName = 'tag_name',
+    Name = 'name',
+    Draft = 'draft',
+    PreRelease = 'prerelease',
+    TargetCommitish = 'target_commitish',
+    CreatedAt = 'created_at',
+    PublishedAt = 'published_at',
+    Body = 'body'
+}
